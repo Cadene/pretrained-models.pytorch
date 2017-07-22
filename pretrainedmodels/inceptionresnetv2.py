5076675f8522d08949b8ff94e2ff61af05292b17
Remi Cadene <remi.cadene@icloud.com>
2017-07-22 14:30:54 +0200

    Fix BN momentum in inception*

--- pretrainedmodels/inceptionresnetv2.py
+++ pretrainedmodels/inceptionresnetv2.py
@@ -34,8 +34,13 @@ class BasicConv2d(nn.Module):
 
     def __init__(self, in_planes, out_planes, kernel_size, stride, padding=0):
         super(BasicConv2d, self).__init__()
-        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, bias=False) # verify bias false
-        self.bn = nn.BatchNorm2d(out_planes, eps=0.001, momentum=0, affine=True)
+        self.conv = nn.Conv2d(in_planes, out_planes,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, bias=False) # verify bias false
+        self.bn = nn.BatchNorm2d(out_planes,
+                                 eps=0.001, # value found in tensorflow
+                                 momentum=0.1, # default pytorch value
+                                 affine=True)
         self.relu = nn.ReLU(inplace=False)
 
     def forward(self, x):
