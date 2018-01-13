f50927ea5bb5a4b36a52e6da9d94a8f9d05614a1
Remi Cadene <remi.cadene@icloud.com>
2018-01-13 12:57:13 +0100

    Fix to support pretrainedmodels.__dict__['nasnetalarge'] instead of pretrainedmodels.models.__dict__['nasnetalarge']

--- pretrainedmodels/__init__.py
+++ pretrainedmodels/__init__.py
@@ -2,3 +2,34 @@ from . import models
 from . import datasets
 from .models.utils import pretrained_settings
 from .models.utils import model_names
+
+# to support pretrainedmodels.__dict__['nasnetalarge']
+# but depreciated
+from .models.fbresnet import fbresnet152
+from .models.bninception import bninception
+from .models.resnext import resnext101_32x4d
+from .models.resnext import resnext101_64x4d
+from .models.inceptionv4 import inceptionv4
+from .models.inceptionresnetv2 import inceptionresnetv2
+from .models.nasnet import nasnetalarge
+from .models.torchvision_models import alexnet
+from .models.torchvision_models import densenet121
+from .models.torchvision_models import densenet169
+from .models.torchvision_models import densenet201
+from .models.torchvision_models import densenet161
+from .models.torchvision_models import resnet18
+from .models.torchvision_models import resnet34
+from .models.torchvision_models import resnet50
+from .models.torchvision_models import resnet101
+from .models.torchvision_models import resnet152
+from .models.torchvision_models import inceptionv3
+from .models.torchvision_models import squeezenet1_0
+from .models.torchvision_models import squeezenet1_1
+from .models.torchvision_models import vgg11
+from .models.torchvision_models import vgg11_bn
+from .models.torchvision_models import vgg13
+from .models.torchvision_models import vgg13_bn
+from .models.torchvision_models import vgg16
+from .models.torchvision_models import vgg16_bn
+from .models.torchvision_models import vgg19_bn
+from .models.torchvision_models import vgg19
