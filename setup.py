a5c2e829621a4edd301aab1547b5064bbdb65623
Remi Cadene <remi.cadene@icloud.com>
2018-01-13 13:00:49 +0100

    Version 0.1.1 to be able to submit on pip server

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',  # Required
+    version='0.1.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
