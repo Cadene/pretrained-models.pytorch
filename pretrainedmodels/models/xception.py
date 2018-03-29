f2ede774b06d2006c02cc6fb0148f719be560038
Remi Cadene <remi.cadene@icloud.com>
2018-03-29 09:58:15 +0200

    Fix last_linear of xception

--- pretrainedmodels/models/xception.py
+++ pretrainedmodels/models/xception.py
@@ -202,7 +202,7 @@ class Xception(nn.Module):
 
         x = F.adaptive_avg_pool2d(x, (1, 1))
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.last_linear(x)
         return x
 
     def forward(self, input):
