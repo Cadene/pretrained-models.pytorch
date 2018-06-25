a53eca27e3892f135dff95bec60d8486e7b33b95
Michaël de Vries <vriesdemichael@gmail.com>
2018-06-25 12:12:07 +0200

    Add tqdm as requirement in setup.py
    "ModuleNotFoundError: No module named 'tqdm'", requirement is currently not installed by pip

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['torch', 'torchvision', 'munch'],  # Optional
+    install_requires=['torch', 'torchvision', 'munch', 'tqdm'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
