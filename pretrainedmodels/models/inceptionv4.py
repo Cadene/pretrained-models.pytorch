51b8e1ebee49597c6e65424111218d9cbe16c705
ekka <ekagra.ranjan@gmail.com>
2019-01-20 12:41:46 +0530

    Updated inceptionV4 to accept different sized images
    The update allows inceptionV4 to process images larger or smaller than prescribed image size (299x299). Will be useful while finetuning or testing on different resolution images.

--- pretrainedmodels/models/inceptionv4.py
+++ pretrainedmodels/models/inceptionv4.py
@@ -298,6 +298,9 @@ class InceptionV4(nn.Module):
         self.last_linear = nn.Linear(1536, num_classes)
 
     def logits(self, features):
+        #Allows image of any size to be processed
+        adaptiveAvgPoolWidth = features.shape[2]
+        x = F.avg_pool2d(features, kernel_size=adaptiveAvgPoolWidth)
         x = self.avg_pool(features)
         x = x.view(x.size(0), -1)
         x = self.last_linear(x)
