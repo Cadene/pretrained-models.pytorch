021d97897c9aa76ec759deff43d341c4fd45d7ba
Remi Cadene <remi.cadene@icloud.com>
2019-04-03 23:18:00 +0200

    Update imagenet_eval.py to torch 1.0.x

--- examples/imagenet_eval.py
+++ examples/imagenet_eval.py
@@ -229,9 +229,9 @@ def validate(val_loader, model, criterion):
 
             # measure accuracy and record loss
             prec1, prec5 = accuracy(output.data, target.data, topk=(1, 5))
-            losses.update(loss.data[0], input.size(0))
-            top1.update(prec1[0], input.size(0))
-            top5.update(prec5[0], input.size(0))
+            losses.update(loss.data.item(), input.size(0))
+            top1.update(prec1.item(), input.size(0))
+            top5.update(prec5.item(), input.size(0))
 
             # measure elapsed time
             batch_time.update(time.time() - end)
