ee42bb0cce63969c020ac35f44e4930fee6cb8cc
Reinier Maat <reiniermaat@msn.com>
2019-05-06 14:44:13 -0400

    Python 2 float/integer compatibility fix

--- pretrainedmodels/models/senet.py
+++ pretrainedmodels/models/senet.py
@@ -189,7 +189,7 @@ class SEResNeXtBottleneck(Bottleneck):
     def __init__(self, inplanes, planes, groups, reduction, stride=1,
                  downsample=None, base_width=4):
         super(SEResNeXtBottleneck, self).__init__()
-        width = math.floor(planes * (base_width / 64)) * groups
+        width = int(math.floor(planes * (base_width / 64)) * groups)
         self.conv1 = nn.Conv2d(inplanes, width, kernel_size=1, bias=False,
                                stride=1)
         self.bn1 = nn.BatchNorm2d(width)
